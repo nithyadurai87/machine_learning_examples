7c5e930d5d15b8032517f5a7168847c3193f3b64
Nithya <Nithya.Duraisamy@tcs.com>
2019-01-31 17:23:54 +0530

    Added

--- Algorithmns/06_multiple_linear_equation.py
+++ Algorithmns/06_multiple_linear_equation.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([1, 800, 2, 15])
-y = [3000000]
+x = np.array([[1, 800, 2, 15],[1, 1200, 3, 1],[1, 2400, 5, 5]])
+y = [3000000,2000000,3500000]
 theta = np.array([100, 1000, 10000, 100000])
 
-print (np.matmul(theta.transpose(),x))
+print (x.dot(theta.transpose()))
 
--- Algorithmns/07_multiple_linear_costfn.py
+++ Algorithmns/07_multiple_linear_costfn.py
@@ -1,9 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([1, 800, 2, 15])
-y = [3000000]
+x = np.array([[1, 800, 2, 15],[1, 1200, 3, 1],[1, 2400, 5, 5]])
+y = np.array([3000000,2000000,3500000])
 theta = np.array([100, 1000, 10000, 100000])
 
-print (np.matmul(theta.transpose(),x))
+predicted_y = x.dot(theta.transpose())
 
+diff = predicted_y - y
+print (np.square(diff))
+
+"""
+
+m = y.size
+sum=0
+for i,j in zip(np.nditer(predicted_y),np.nditer(y)):
+	print (i-j)
+	print (type(i-j))
+	print (np.array((i-j), dtype='int32')**2)
+"""
+	
